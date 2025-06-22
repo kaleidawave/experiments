9d98b58115f76f0a685bab9c930f9034f3398188
Ben <kaleidawave@gmail.com>
2025-06-22 08:18:25 +0100

    Item and scan implementation

--- language-tool/src/main.rs
+++ language-tool/src/main.rs
@@ -22,10 +22,54 @@ fn main() {
 
     eprintln!("initiated parser");
 
-    let source_code = "let x = 2;";
+    let source_code = "
+enum X {
+    Some(Box<X>),
+    None
+}
+
+let x = X::None;
+"
+    .trim();
+
+    let list = scan(source_code, &mut parser);
+    println!("--- source_code ---\n{source_code}\n---");
+    for item in list {
+        println!("{item:?}");
+    }
+}
+
+#[derive(Debug)]
+struct Item<'a> {
+    pub grammar_name: &'static str,
+    pub source: &'a str,
+    pub range: (usize, usize),
+}
+
+fn scan<'a>(source_code: &'a str, parser: &mut Parser) -> Vec<Item<'a>> {
+    fn flat_walk<'a, 's>(
+        source_code: &'s str,
+        node: tree_sitter::Node<'a>,
+        list: &mut Vec<Item<'s>>,
+    ) {
+        if node.child_count() > 0 {
+            let mut walker = node.walk();
+            for child in node.children(&mut walker) {
+                flat_walk(source_code, child, list);
+            }
+        } else {
+            list.push(Item {
+                grammar_name: node.grammar_name(),
+                source: &source_code[node.start_byte()..node.end_byte()],
+                range: (node.start_byte(), node.end_byte()),
+            });
+        }
+    }
 
     let tree = parser.parse(source_code, None).unwrap();
     let root = tree.root_node();
 
-    eprintln!("root: {:?}", root.kind());
+    let mut list = Vec::new();
+    flat_walk(source_code, root, &mut list);
+    list
 }
