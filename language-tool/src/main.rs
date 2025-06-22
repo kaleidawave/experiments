e985a2294cbfef30b15273ce4a7ca7e536ece4ad
Ben <kaleidawave@gmail.com>
2025-06-22 09:14:30 +0100

    More fixes to make OS agnostic

--- language-tool/src/main.rs
+++ language-tool/src/main.rs
@@ -5,15 +5,15 @@ use tree_sitter_language::LanguageFn;
 fn main() {
     let language = "rust";
     eprintln!("loading {language}");
-    let module = format!("languages/{language}");
+    let module = std::path::Path::new("languages").join(library_filename(&language));
     let function_name = format!("tree_sitter_{language}");
-    let lib = unsafe { Library::new(library_filename(&module)).unwrap() };
+    let lib = unsafe { Library::new(&module).unwrap() };
     let function = unsafe {
         lib.get::<Symbol<extern "C" fn() -> *const ()>>(function_name.as_bytes())
             .unwrap()
     };
     let language_fn = unsafe { LanguageFn::from_raw(**function) };
-    eprintln!("loaded {function_name} from {module}");
+    eprintln!("loaded {function_name} from {module}", module=module.display());
 
     let mut parser = Parser::new();
     parser
