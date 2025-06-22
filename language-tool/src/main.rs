8613950d4e83758fe21521c89aeac008c43fe4e4
Ben <kaleidawave@gmail.com>
2025-06-22 08:49:46 +0100

    OS agnostic

--- language-tool/src/main.rs
+++ language-tool/src/main.rs
@@ -1,13 +1,13 @@
-use libloading::{Library, Symbol};
+use libloading::{Library, Symbol, library_filename};
 use tree_sitter::{Language, Parser};
 use tree_sitter_language::LanguageFn;
 
 fn main() {
     let language = "rust";
     eprintln!("loading {language}");
-    let module = format!("languages/{language}.dll");
+    let module = format!("languages/{language}");
     let function_name = format!("tree_sitter_{language}");
-    let lib = unsafe { Library::new(&module).unwrap() };
+    let lib = unsafe { Library::new(library_filename(&module)).unwrap() };
     let function = unsafe {
         lib.get::<Symbol<extern "C" fn() -> *const ()>>(function_name.as_bytes())
             .unwrap()
