4a617e04683ec4514ab7c2f27300e1a10d4c6d67
Ben <kaleidawave@gmail.com>
2025-07-01 20:01:07 +0100

    Add timeout thing

--- stdout-stderr-utilities/lib.rs
+++ stdout-stderr-utilities/lib.rs
@@ -1,9 +1,10 @@
 #![warn(clippy::pedantic)]
 
-use std::io::{self, BufRead, BufReader, Read};
+use std::io::{self, BufRead, BufReader, Read, Write};
 use std::process::{self, Child, Command, ExitStatus, Stdio};
 use std::sync;
 use std::thread;
+use std::time::Duration;
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub enum Channel {
@@ -80,7 +81,6 @@ impl CommandOut {
                     let thread = s.spawn(|| {
                         let mut stderr_buf = String::new();
                         for line in stderr.lines().map_while(Result::ok) {
-                            dbg!(&line);
                             if line == "please finish" {
                                 break;
                             }
@@ -92,7 +92,6 @@ impl CommandOut {
 
                     let mut stdout_buf = String::new();
                     for line in stdout.lines().map_while(Result::ok) {
-                        dbg!(&line);
                         if cb(&line) {
                             break;
                         }
@@ -100,12 +99,9 @@ impl CommandOut {
                         stdout_buf.push('\n');
                     }
 
-                    use std::io::Write;
                     writeln!(stderr_writer, "please finish").unwrap();
 
-                    // dbg!();
                     let stderr_buf = thread.join().unwrap();
-                    // dbg!();
 
                     (stdout_buf, stderr_buf)
                 });
@@ -134,19 +130,14 @@ impl CommandOut {
                 let mut stdout_buf = String::new();
                 let mut stderr_buf = String::new();
 
-                dbg!();
                 stdout.read_to_string(&mut stdout_buf)?;
-                dbg!();
                 drop(stderr_writer);
                 stderr.read_to_string(&mut stderr_buf)?;
-                dbg!();
 
                 stdout_buf.truncate(stdout_buf.trim_end().len());
                 stderr_buf.truncate(stderr_buf.trim_end().len());
 
-                dbg!();
                 let status = child.wait()?;
-                dbg!();
 
                 Ok((stdout_buf, stderr_buf, status))
             }
@@ -193,6 +184,63 @@ impl CommandOut {
             panic!("cannot read merged independently");
         }
     }
+}
+
+const TIMEOUT_MESSAGE: &[u8] = b"=== TIMEOUT ===";
+pub const END_MESSAGE: &[u8] = b"=== end ===";
+
+pub fn run_timeout<O>(
+    timeout: Duration,
+    command: impl FnOnce(std::io::PipeWriter) -> O,
+) -> Result<(O, Vec<u8>), Box<dyn std::error::Error>> {
+    use std::sync::{Arc, Condvar, Mutex};
+
+    let (ping_rx, ping_tx) = io::pipe()?;
+
+    let mut ping_tx2 = ping_tx.try_clone().expect("could not clone writer");
+
+    let main_pair = Arc::new((Mutex::new(false), Condvar::new()));
+    let thread_pair = Arc::clone(&main_pair);
+
+    let _monitor = thread::spawn(move || {
+        let (lock, cvar) = &*thread_pair;
+        let completed = lock.lock().unwrap();
+        let result = cvar.wait_timeout(completed, timeout);
+        if let Ok((_finished, result)) = result {
+            if result.timed_out() {
+                io::Write::write_all(&mut ping_tx2, TIMEOUT_MESSAGE).unwrap();
+            }
+        }
+    });
+
+    // My command
+    let out = command(ping_tx);
+
+    let (lock, cvar) = &*main_pair;
+
+    let mut reader = BufReader::new(ping_rx);
+    let mut buf = Vec::new();
+    while let Ok(bytes) = reader.fill_buf() {
+        if bytes.ends_with(TIMEOUT_MESSAGE) {
+            return Err(String::from("Timed out").into());
+        }
+        if bytes.ends_with(END_MESSAGE) {
+            {
+                *lock.lock().unwrap() = true;
+                // We notify the condvar that the value has changed.
+                cvar.notify_one();
+            }
+            break;
+        }
+        let to_continue = bytes.len();
+        buf.extend_from_slice(bytes);
+        if to_continue == 0 {
+            break;
+        }
+        reader.consume(to_continue);
+    }
+
+    Ok((out, buf))
 }
 
 #[cfg(test)]
@@ -253,5 +301,38 @@ mod tests {
             (stdout.as_str(), stderr.as_str()),
             ("Hiya\ntest\nend", "Hello")
         );
+    }
+
+    #[test]
+    fn timeout() {
+        let timeout = Duration::from_millis(1000);
+
+        type ThreadResult<T> = thread::JoinHandle<io::Result<T>>;
+
+        let out = run_timeout::<ThreadResult<_>>(timeout, |mut writer| {
+            thread::spawn(move || {
+                writer.write_all(&[1, 2, 3])?;
+                thread::sleep(Duration::from_millis(100));
+                writer.write_all(&[4, 5, 6])?;
+                thread::sleep(Duration::from_millis(100));
+                writer.write_all(END_MESSAGE)?;
+                Ok(())
+            })
+        });
+
+        assert_eq!(out.expect("timed out").1, &[1, 2, 3, 4, 5, 6]);
+
+        let out = run_timeout::<ThreadResult<_>>(timeout, |mut writer| {
+            thread::spawn(move || {
+                writer.write_all(&[1, 2, 3])?;
+                thread::sleep(Duration::from_millis(1000));
+                writer.write_all(&[4, 5, 6])?;
+                thread::sleep(Duration::from_millis(3000));
+                writer.write_all(END_MESSAGE)?;
+                Ok(())
+            })
+        });
+
+        assert!(out.is_err());
     }
 }
