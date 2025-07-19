800c2313028a06cc25ab354a876bf05e333ace41
Ben <kaleidawave@gmail.com>
2025-07-19 12:38:06 +0100

    Parse update

--- general-parser/examples/parse.rs
+++ general-parser/examples/parse.rs
@@ -1,11 +1,6 @@
 use general_parser::{Configuration, BinaryOperator, Expression};
 
 fn main() {
-	let sources: &[&str] = &[
-		"(x (a * b) (d * 2 + e))",
-		"(x (a b) (c d e))"
-	];
-
 	let configuration = Configuration {
 		binary_operators: vec![
 			BinaryOperator { representation: "*", precedence: 4 },
@@ -14,12 +9,22 @@ fn main() {
 		..Default::default()
 	};
 
-
-	for source in sources {
+	if let Some(source) = std::env::args().nth(1) {
 		let allocator = bumpalo::Bump::new();
 		let expression: Expression<&str> =
-			Expression::from_string(source, &configuration, &allocator);
+			Expression::from_string(&source, &configuration, &allocator);
 		eprintln!("{expression:#?}");
+	} else {
+		let sources: &[&str] = &[
+			"(x (a * b) (d * 2 + e))",
+			"(x (a b) (c d e))"
+		];
+	
+		for source in sources {
+			let allocator = bumpalo::Bump::new();
+			let expression: Expression<&str> =
+				Expression::from_string(source, &configuration, &allocator);
+			eprintln!("{expression:#?}");
+		}
 	}
-
 }
