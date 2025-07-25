3e3f7da44607ca7944f81d56e847809678977df7
Ben <kaleidawave@gmail.com>
2025-07-25 18:02:09 +0100

    Add specification tests

--- general-parser/examples/parse.rs
+++ general-parser/examples/parse.rs
@@ -1,6 +1,13 @@
-use general_parser::{BinaryOperator, Configuration, Expression};
+use general_parser::{BinaryOperator, Configuration, Expression, UnaryOperator};
 
 fn main() {
+	let arg = std::env::args().nth(1);
+
+	if let Some("--interactive") = arg.as_deref() {
+		run_interactive();
+		return;
+	}
+
 	let configuration = Configuration {
 		binary_operators: vec![
 			BinaryOperator { representation: "*", precedence: 4 },
@@ -23,5 +30,87 @@ fn main() {
 				Expression::from_string(source, &configuration, &allocator);
 			eprintln!("{expression:#?}");
 		}
+	}
+}
+
+fn run_interactive() {
+	use std::io::{BufRead, stdin};
+	let stdin = stdin();
+	let mut buf = Vec::new();
+
+	println!("start");
+
+	for line in stdin.lock().lines().map_while(Result::ok) {
+		if line == "close" {
+			if !buf.is_empty() {
+				eprintln!("no end to message {buf:?}");
+			}
+			break;
+		}
+
+		if line == "end" {
+			let output = String::from_utf8_lossy(&buf);
+
+			let (configuration, source) = if let Some((config, source)) = output.split_once("\n---")
+			{
+				let mut configuration = Configuration::default();
+				for line in config.lines() {
+					let (adjacent, operation) = if let Some(rest) = line.strip_suffix(" (adjacent)")
+					{
+						(true, rest)
+					} else {
+						(false, line)
+					};
+					let (syntax, precedence) = operation.split_once(' ').unwrap();
+					let precedence: u8 = precedence.parse().expect("invalid precedence");
+					if let Some(syntax) = syntax.strip_prefix('_') {
+						if let Some(representation) = syntax.strip_suffix('_') {
+							let operator = BinaryOperator { representation, precedence };
+							if adjacent {
+								configuration.adjacency = Some(general_parser::Adjacency {
+									operator,
+									functions: Vec::new(),
+								});
+							} else {
+								configuration.binary_operators.push(operator);
+							}
+						} else {
+							let representation = syntax;
+							configuration
+								.postfix_unary_operators
+								.push(UnaryOperator { representation, precedence });
+						}
+					} else {
+						let representation = syntax.strip_suffix('_').unwrap();
+						configuration
+							.prefix_unary_operators
+							.push(UnaryOperator { representation, precedence });
+					}
+				}
+				(configuration, source)
+			} else {
+				(Configuration::default(), &*output)
+			};
+
+			// eprintln!("{configuration:?} {source}");
+
+			{
+				let allocator = bumpalo::Bump::new();
+				let expression: Expression<&str> =
+					Expression::from_string(&source, &configuration, &allocator);
+				println!("{expression:#?}");
+			}
+
+			// if let Err(error) = out {
+			//     println!("Error: {error:?}");
+			// }
+
+			println!("end");
+			buf.clear();
+			continue;
+		}
+
+		buf.extend_from_slice(line.as_bytes());
+		buf.push(b'\n');
 	}
 }
---
+++ general-parser/specification.md
@@ -0,0 +1,98 @@
+### Operation
+
+```
+(x (a b) (c d))
+```
+
+```
+Expression {
+    on: "x",
+    arguments: [
+        Expression {
+            on: "a",
+            arguments: [
+                Expression {
+                    on: "b",
+                    arguments: [],
+                },
+            ],
+        },
+        Expression {
+            on: "c",
+            arguments: [
+                Expression {
+                    on: "d",
+                    arguments: [],
+                },
+            ],
+        },
+    ],
+}
+```
+
+### Operations
+
+```
+_*_ 4
+_+_ 3
+---
+x * y + 2
+```
+
+```
+Expression {
+    on: "+",
+    arguments: [
+        Expression {
+            on: "*",
+            arguments: [
+                Expression {
+                    on: "x",
+                    arguments: [],
+                },
+                Expression {
+                    on: "y",
+                    arguments: [],
+                },
+            ],
+        },
+        Expression {
+            on: "2",
+            arguments: [],
+        },
+    ],
+}
+```
+
+### Adjacency
+
+```
+_*_ 4 (adjacent)
+---
+3xy
+```
+
+```
+Expression {
+    on: "*",
+    arguments: [
+        Expression {
+            on: "*",
+            arguments: [
+                Expression {
+                    on: "3",
+                    arguments: [],
+                },
+                Expression {
+                    on: "x",
+                    arguments: [],
+                },
+            ],
+        },
+        Expression {
+            on: "y",
+            arguments: [],
+        },
+    ],
+}
+```
