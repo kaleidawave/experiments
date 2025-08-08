c24ee6c59c0c407af61247c2a2d746d3e0a57e9c
Ben <kaleidawave@gmail.com>
2025-08-08 14:05:47 +0100

    Small tweaks

--- general-parser/tests/specification.rs
+++ general-parser/tests/specification.rs
@@ -1,6 +1,4 @@
 fn main() -> std::process::ExitCode {
-	dbg!(std::env::args().collect::<Vec<_>>());
-
 	let output = std::process::Command::new("spectra")
 		.arg("check")
 		.arg("./specification.md")
