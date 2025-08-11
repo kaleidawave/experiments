fa01670d086c0afc18f4af8b42c2aeaf4008c229
Ben <kaleidawave@gmail.com>
2025-08-11 17:13:49 +0100

    Fixes

--- pseudo-random/examples/random.rs
+++ pseudo-random/examples/random.rs
@@ -1,7 +1,9 @@
 use std::time::SystemTime;
 
 fn main() {
-	let elapsed = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
-	let value = elapsed.as_micros() % 1000;
-	println!("{value}");
+    let elapsed = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap();
+    let value = elapsed.as_micros() % 1000;
+    println!("{value}");
 }
--- pseudo-random/src/algorithms/mulberry32.rs
+++ pseudo-random/src/algorithms/mulberry32.rs
@@ -1,13 +1,13 @@
 pub struct Mulberry32 {
-	pub seed: u32
+    pub seed: u32,
 }
 
 impl Mulberry32 {
-	pub fn next(&mut self) -> u32 {
-		self.seed = self.seed.wrapping_add(0x6D2B79F5);
-		let mut t = self.seed;
-		t = (t ^ t.wrapping_shr(15)).wrapping_mul(t | 1);
-		t ^= t.wrapping_add(t ^ t.wrapping_shr(7).wrapping_mul(t | 61));
-		t ^ t.wrapping_shr(14)
-	}
+    pub fn next_value(&mut self) -> u32 {
+        self.seed = self.seed.wrapping_add(0x6D2B79F5);
+        let mut t = self.seed;
+        t = (t ^ t.wrapping_shr(15)).wrapping_mul(t | 1);
+        t ^= t.wrapping_add(t ^ t.wrapping_shr(7).wrapping_mul(t | 61));
+        t ^ t.wrapping_shr(14)
+    }
 }
