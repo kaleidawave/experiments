bc403d344da38e0dcf10a0fb34b19920fcf48575
Ben <kaleidawave@gmail.com>
2025-08-20 08:23:33 +0100

    Add sending headers, parsing response codes and non-utf8 responses

--- simple-http-requester/lib.rs
+++ simple-http-requester/lib.rs
@@ -4,93 +4,330 @@ use native_tls::{TlsConnector, TlsStream};
 use std::io::{Read, Write};
 use std::net::TcpStream;
 
-pub fn make_request(root: &str, path: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let mut stream = initiate_stream(root, path)?;
+#[allow(non_snake_case)]
+pub mod HTTP {
+    #[derive(Debug)]
+    pub struct Request<'a> {
+        pub method: &'a str,
+        pub path: &'a str,
+        pub headers: Headers<'a>,
+        pub body: &'a [u8],
+    }
 
-    let mut response = String::new();
-    stream.read_to_string(&mut response)?;
+    #[derive(PartialEq, Eq, Clone, Copy)]
+    pub struct ResponseCode(pub u16);
 
-    // {
-    //     let mut file = std::fs::File::create("private/total.http")?;
-    //     file.write_all(response.as_bytes())?;
-    // }
+    impl ResponseCode {
+        pub const CONTINUE: ResponseCode = ResponseCode(100);
+        pub const SWITCHING_PROTOCOLS: ResponseCode = ResponseCode(101);
+        pub const PROCESSING: ResponseCode = ResponseCode(102);
+        pub const EARLY_HINTS: ResponseCode = ResponseCode(103);
 
-    // Skip headers
-    let mut lines = response.lines();
+        pub const OK: ResponseCode = ResponseCode(200);
+        pub const CREATED: ResponseCode = ResponseCode(201);
+        pub const ACCEPTED: ResponseCode = ResponseCode(202);
+        pub const NON_AUTHORITATIVE_INFORMATION: ResponseCode = ResponseCode(203);
+        pub const NO_CONTENT: ResponseCode = ResponseCode(204);
+        pub const RESET_CONTENT: ResponseCode = ResponseCode(205);
+        pub const PARTIAL_CONTENT: ResponseCode = ResponseCode(206);
+        pub const MULTI_STATUS: ResponseCode = ResponseCode(207);
+        pub const ALREADY_REPORTED: ResponseCode = ResponseCode(208);
+        pub const IM_USED: ResponseCode = ResponseCode(226);
+
+        pub const MULTIPLE_CHOICES: ResponseCode = ResponseCode(300);
+        pub const MOVED_PERMANENTLY: ResponseCode = ResponseCode(301);
+        pub const FOUND: ResponseCode = ResponseCode(302);
+        pub const SEE_OTHER: ResponseCode = ResponseCode(303);
+        pub const NOT_MODIFIED: ResponseCode = ResponseCode(304);
+        pub const TEMPORARY_REDIRECT: ResponseCode = ResponseCode(307);
+        pub const PERMANENT_REDIRECT: ResponseCode = ResponseCode(308);
+
+        pub const BAD_REQUEST: ResponseCode = ResponseCode(400);
+        pub const UNAUTHORIZED: ResponseCode = ResponseCode(401);
+        pub const PAYMENT_REQUIRED: ResponseCode = ResponseCode(402);
+        pub const FORBIDDEN: ResponseCode = ResponseCode(403);
+        pub const NOT_FOUND: ResponseCode = ResponseCode(404);
+        pub const METHOD_NOT_ALLOWED: ResponseCode = ResponseCode(405);
+        pub const NOT_ACCEPTABLE: ResponseCode = ResponseCode(406);
+        pub const PROXY_AUTHENTICATION_REQUIRED: ResponseCode = ResponseCode(407);
+        pub const REQUEST_TIMEOUT: ResponseCode = ResponseCode(408);
+        pub const CONFLICT: ResponseCode = ResponseCode(409);
+        pub const GONE: ResponseCode = ResponseCode(410);
+        pub const LENGTH_REQUIRED: ResponseCode = ResponseCode(411);
+        pub const PRECONDITION_FAILED: ResponseCode = ResponseCode(412);
+        pub const CONTENT_TOO_LARGE: ResponseCode = ResponseCode(413);
+        pub const URI_TOO_LONG: ResponseCode = ResponseCode(414);
+        pub const UNSUPPORTED_MEDIA_TYPE: ResponseCode = ResponseCode(415);
+        pub const RANGE_NOT_SATISFIABLE: ResponseCode = ResponseCode(416);
+        pub const EXPECTATION_FAILED: ResponseCode = ResponseCode(417);
+        pub const IM_A_TEAPOT: ResponseCode = ResponseCode(418);
+        pub const MISDIRECTED_REQUEST: ResponseCode = ResponseCode(421);
+        pub const UNPROCESSABLE_CONTENT: ResponseCode = ResponseCode(422);
+        pub const LOCKED: ResponseCode = ResponseCode(423);
+        pub const FAILED_DEPENDENCY: ResponseCode = ResponseCode(424);
+        pub const TOO_EARLY: ResponseCode = ResponseCode(425);
+        pub const UPGRADE_REQUIRED: ResponseCode = ResponseCode(426);
+        pub const PRECONDITION_REQUIRED: ResponseCode = ResponseCode(428);
+        pub const TOO_MANY_REQUESTS: ResponseCode = ResponseCode(429);
+        pub const REQUEST_HEADER_FIELDS_TOO_LARGE: ResponseCode = ResponseCode(431);
+        pub const UNAVAILABLE_FOR_LEGAL_REASONS: ResponseCode = ResponseCode(451);
+
+        pub const INTERNAL_SERVER_ERROR: ResponseCode = ResponseCode(500);
+        pub const NOT_IMPLEMENTED: ResponseCode = ResponseCode(501);
+        pub const BAD_GATEWAY: ResponseCode = ResponseCode(502);
+        pub const SERVICE_UNAVAILABLE: ResponseCode = ResponseCode(503);
+        pub const GATEWAY_TIMEOUT: ResponseCode = ResponseCode(504);
+        pub const HTTP_VERSION_NOT_SUPPORTED: ResponseCode = ResponseCode(505);
+        pub const VARIANT_ALSO_NEGOTIATES: ResponseCode = ResponseCode(506);
+        pub const INSUFFICIENT_STORAGE: ResponseCode = ResponseCode(507);
+        pub const LOOP_DETECTED: ResponseCode = ResponseCode(508);
+        pub const NOT_EXTENDED: ResponseCode = ResponseCode(510);
+        pub const NETWORK_AUTHENTICATION_REQUIRED: ResponseCode = ResponseCode(511);
+
+        pub fn from_line(item: &str) -> Result<Self, &str> {
+            Ok(match item {
+                "100 Continue" => Self::CONTINUE,
+                "101 Switching Protocols" => Self::SWITCHING_PROTOCOLS,
+                "102 Processing" => Self::PROCESSING,
+                "103 Early Hints" => Self::EARLY_HINTS,
+                "200 OK" => Self::OK,
+                "201 Created" => Self::CREATED,
+                "202 Accepted" => Self::ACCEPTED,
+                "203 Non-Authoritative Information" => Self::NON_AUTHORITATIVE_INFORMATION,
+                "204 No Content" => Self::NO_CONTENT,
+                "205 Reset Content" => Self::RESET_CONTENT,
+                "206 Partial Content" => Self::PARTIAL_CONTENT,
+                "207 Multi-Status" => Self::MULTI_STATUS,
+                "208 Already Reported" => Self::ALREADY_REPORTED,
+                "226 IM Used" => Self::IM_USED,
+                "300 Multiple Choices" => Self::MULTIPLE_CHOICES,
+                "301 Moved Permanently" => Self::MOVED_PERMANENTLY,
+                "302 Found" => Self::FOUND,
+                "303 See Other" => Self::SEE_OTHER,
+                "304 Not Modified" => Self::NOT_MODIFIED,
+                "307 Temporary Redirect" => Self::TEMPORARY_REDIRECT,
+                "308 Permanent Redirect" => Self::PERMANENT_REDIRECT,
+                "400 Bad Request" => Self::BAD_REQUEST,
+                "401 Unauthorized" => Self::UNAUTHORIZED,
+                "402 Payment Required" => Self::PAYMENT_REQUIRED,
+                "403 Forbidden" => Self::FORBIDDEN,
+                "404 Not Found" => Self::NOT_FOUND,
+                "405 Method Not Allowed" => Self::METHOD_NOT_ALLOWED,
+                "406 Not Acceptable" => Self::NOT_ACCEPTABLE,
+                "407 Proxy Authentication Required" => Self::PROXY_AUTHENTICATION_REQUIRED,
+                "408 Request Timeout" => Self::REQUEST_TIMEOUT,
+                "409 Conflict" => Self::CONFLICT,
+                "410 Gone" => Self::GONE,
+                "411 Length Required" => Self::LENGTH_REQUIRED,
+                "412 Precondition Failed" => Self::PRECONDITION_FAILED,
+                "413 Content Too Large" => Self::CONTENT_TOO_LARGE,
+                "414 URI Too Long" => Self::URI_TOO_LONG,
+                "415 Unsupported Media Type" => Self::UNSUPPORTED_MEDIA_TYPE,
+                "416 Range Not Satisfiable" => Self::RANGE_NOT_SATISFIABLE,
+                "417 Expectation Failed" => Self::EXPECTATION_FAILED,
+                "418 I'm a teapot" => Self::IM_A_TEAPOT,
+                "421 Misdirected Request" => Self::MISDIRECTED_REQUEST,
+                "422 Unprocessable Content" => Self::UNPROCESSABLE_CONTENT,
+                "423 Locked" => Self::LOCKED,
+                "424 Failed Dependency" => Self::FAILED_DEPENDENCY,
+                "425 Too Early" => Self::TOO_EARLY,
+                "426 Upgrade Required" => Self::UPGRADE_REQUIRED,
+                "428 Precondition Required" => Self::PRECONDITION_REQUIRED,
+                "429 Too Many Requests" => Self::TOO_MANY_REQUESTS,
+                "431 Request Header Fields Too Large" => Self::REQUEST_HEADER_FIELDS_TOO_LARGE,
+                "451 Unavailable For Legal Reasons" => Self::UNAVAILABLE_FOR_LEGAL_REASONS,
+                "500 Internal Server Error" => Self::INTERNAL_SERVER_ERROR,
+                "501 Not Implemented" => Self::NOT_IMPLEMENTED,
+                "502 Bad Gateway" => Self::BAD_GATEWAY,
+                "503 Service Unavailable" => Self::SERVICE_UNAVAILABLE,
+                "504 Gateway Timeout" => Self::GATEWAY_TIMEOUT,
+                "505 HTTP Version Not Supported" => Self::HTTP_VERSION_NOT_SUPPORTED,
+                "506 Variant Also Negotiates" => Self::VARIANT_ALSO_NEGOTIATES,
+                "507 Insufficient Storage" => Self::INSUFFICIENT_STORAGE,
+                "508 Loop Detected" => Self::LOOP_DETECTED,
+                "510 Not Extended" => Self::NOT_EXTENDED,
+                "511 Network Authentication Required" => Self::NETWORK_AUTHENTICATION_REQUIRED,
+                item => {
+                    return Err(item);
+                }
+            })
+        }
+    }
+
+    pub struct Response<'a> {
+        pub code: ResponseCode,
+        pub headers: Headers<'a>,
+        pub body: std::borrow::Cow<'a, [u8]>,
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct Headers<'a>(pub std::borrow::Cow<'a, str>);
+
+    impl Headers<'static> {
+        #[must_use]
+        pub fn empty() -> Headers<'static> {
+            Headers(std::borrow::Cow::Borrowed(""))
+        }
+
+        #[must_use]
+        pub fn from_raw(on: Vec<u8>) -> Result<Headers<'static>, ()> {
+            if let Ok(headers) = String::from_utf8(on) {
+                Ok(Headers::from_string(headers))
+            } else {
+                Err(())
+            }
+        }
+
+        #[must_use]
+        pub fn from_string(on: String) -> Headers<'static> {
+            Headers(std::borrow::Cow::Owned(on))
+        }
+    }
+
+    impl<'a, 'b> IntoIterator for &'b Headers<'a> {
+        type Item = (&'b str, &'b str);
+        type IntoIter = HeaderIter<'b>;
+
+        fn into_iter(self) -> Self::IntoIter {
+            HeaderIter(self.0.lines())
+        }
+    }
+
+    pub struct HeaderIter<'a>(pub(super) std::str::Lines<'a>);
+
+    impl<'a> Iterator for HeaderIter<'a> {
+        type Item = (&'a str, &'a str);
+
+        fn next(&mut self) -> Option<Self::Item> {
+            let row = self.0.next()?;
+            // TODO what if `None` here?
+            let (key, value) = row.split_once(':')?;
+            Some((key, value.trim()))
+        }
+    }
+}
+
+pub fn make_request(
+    root: &str,
+    path: &str,
+    headers: &HTTP::Headers<'_>,
+) -> Result<HTTP::Response<'static>, Box<dyn std::error::Error>> {
+    let mut stream = initiate_stream(root, path, headers)?;
+
+    let mut response = Vec::new();
+    stream.read_to_end(&mut response)?;
+
     let mut chunked = false;
 
-    for line in lines.by_ref() {
-        if let Some(location) = line.strip_prefix("Location: ") {
-            eprintln!("Redirect = {location}");
-        }
-        if let Some(transfer_encoding) = line.strip_prefix("Transfer-Encoding: ") {
-            chunked = transfer_encoding == "chunked";
-        }
+    // TODO from first line
+    let mut code = None;
 
-        if line.is_empty() {
-            break;
-        }
-    }
+    let mut headers = None;
 
-    if let Some(next_line) = lines.next() {
-        // TODO sub_ptr https://github.com/rust-lang/rust/issues/95892
-        let headers_end = next_line.as_ptr() as usize - response.as_ptr() as usize;
-        // WIP
-        let body = response[headers_end..].to_owned();
+    let mut last = 0;
+    let mut headers_start = 0;
 
-        let body = if chunked {
-            let (chunk_length_str, rest) = body.split_once("\r\n").expect("no chunk length");
-            let first_size =
-                u64::from_str_radix(chunk_length_str, 16).expect("invalid chunk length") as usize;
-            let mut size = first_size;
+    for (idx, _) in response.iter().enumerate() {
+        if response[idx..].starts_with(b"\r\n") {
+            let line = str::from_utf8(&response[last..idx]).unwrap();
+            last = idx + b"\r\n".len();
 
-            let mut new_body = rest[..size].to_owned();
-            let mut pos = chunk_length_str.len() + "\r\n".len() + first_size + "\r\n".len();
-
-            while size > 0 && pos < body.len() {
-                let (chunk_length_str, rest) =
-                    body[pos..].split_once("\r\n").expect("no chunk length");
-                if chunk_length_str.is_empty() {
-                    break;
+            if code.is_none() {
+                if let Some((_method, item)) = line.split_once(' ') {
+                    code = Some(HTTP::ResponseCode::from_line(item).unwrap());
+                } else {
+                    panic!("{line}");
                 }
-                size = u64::from_str_radix(chunk_length_str, 16).expect("invalid chunk length")
-                    as usize;
-                pos += chunk_length_str.len() + "\r\n".len() + size + "\r\n".len();
-                new_body.push_str(&rest[..size]);
+                headers_start = last;
             }
-            new_body
-        } else {
-            body
-        };
 
-        // {
-        //     let mut file = std::fs::File::create("private/out.html")?;
-        //     file.write_all(body.as_bytes())?;
-        // }
+            if let Some(transfer_encoding) = line.strip_prefix("Transfer-Encoding: ") {
+                chunked = transfer_encoding == "chunked";
+            }
 
-        Ok(body)
-    } else {
-        Err(Box::<dyn std::error::Error>::from(String::from("No body")))
+            if line.is_empty() {
+                let h = HTTP::Headers::from_raw(response[headers_start..idx].to_owned()).unwrap();
+                headers = Some(h);
+                break;
+            }
+        }
     }
+
+    let code = code.unwrap();
+    let headers = headers.unwrap();
+
+    let body = response[last..].to_owned();
+
+    let body = if chunked {
+        let mark = body
+            .windows(2)
+            .position(|item| item[0] == b'\r' && item[1] == b'\n')
+            .expect("no chunk length");
+
+        let (chunk_length_str, rest) = body.split_at(mark);
+        let chunk_length_str = str::from_utf8(chunk_length_str).unwrap();
+        let rest = &rest[2..];
+
+        let first_size =
+            u64::from_str_radix(chunk_length_str, 16).expect("invalid chunk length") as usize;
+
+        let mut size = first_size;
+
+        let mut new_body = rest[..size].to_owned();
+        let mut pos = chunk_length_str.len() + b"\r\n".len() + first_size + b"\r\n".len();
+
+        while size > 0 && pos < body.len() {
+            let mark = body[pos..]
+                .windows(2)
+                .position(|item| item[0] == b'\r' && item[1] == b'\n')
+                .expect("no chunk length");
+            let (chunk_length_str, rest) = body.split_at(mark);
+            let chunk_length_str = str::from_utf8(chunk_length_str).unwrap();
+            let rest = &rest[2..];
+            if chunk_length_str.is_empty() {
+                break;
+            }
+
+            size =
+                u64::from_str_radix(chunk_length_str, 16).expect("invalid chunk length") as usize;
+            pos += chunk_length_str.len() + b"\r\n".len() + size + b"\r\n".len();
+            new_body.extend_from_slice(&rest[..size]);
+        }
+        new_body
+    } else {
+        body
+    };
+
+    let body = std::borrow::Cow::Owned(body);
+
+    let response = HTTP::Response {
+        code,
+        headers,
+        body,
+    };
+
+    Ok(response)
 }
 
 fn initiate_stream(
     root: &str,
     path: &str,
+    headers: &HTTP::Headers<'_>,
 ) -> Result<TlsStream<TcpStream>, Box<dyn std::error::Error>> {
     // TODO 443
     let url = format!("{root}:443");
     let tcp_stream = TcpStream::connect(url)?;
     let connector = TlsConnector::new()?;
     let mut tls_stream = connector.connect(root, tcp_stream)?;
-    let request = format!(
+    let base_request = format!(
         "GET /{path} HTTP/1.1\r\n\
 	Host: {root}\r\n\
 	Connection: close\r\n\
 	User-Agent: yes\r\n"
     );
 
-    tls_stream.write_all(request.as_bytes())?;
+    tls_stream.write_all(base_request.as_bytes())?;
+    tls_stream.write_all(headers.0.as_bytes())?;
     tls_stream.write_all(b"\r\n")?;
 
     Ok(tls_stream)
