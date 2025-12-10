3116ec492b72744f48f87ea030dbe8b5dcb2156f
Ben <kaleidawave@gmail.com>
2025-12-10 10:29:45 +0000

    HTTP module to lowercase

--- mashrl/lib.rs
+++ mashrl/lib.rs
@@ -3,14 +3,13 @@
 use native_tls::{TlsConnector, TlsStream};
 use std::net::TcpStream;
 
-#[allow(non_snake_case)]
-pub mod HTTP;
+pub mod http;
 
 fn write_request<T: std::io::Read, S: std::io::Write>(
-    request: &HTTP::Request<'_, T>,
+    request: &http::Request<'_, T>,
     mut stream: S,
 ) -> Result<S, Box<dyn std::error::Error>> {
-    let HTTP::Request {
+    let http::Request {
         method,
         root,
         path,
@@ -21,7 +20,7 @@ fn write_request<T: std::io::Read, S: std::io::Write>(
     let method: &str = method.0;
 
     let base = format!(
-        "{method} /{path} HTTP/1.1\r\n\
+        "{method} /{path} http/1.1\r\n\
 	Host: {root}\r\n\
 	Connection: close\r\n"
     );
@@ -36,9 +35,9 @@ fn write_request<T: std::io::Read, S: std::io::Write>(
     Ok(stream)
 }
 
-/// Start the HTTP request, sending [`HTTP::Headers<'_>`]
+/// Start the http request, sending [`http::Headers<'_>`]
 fn initiate_stream_tls<T: std::io::Read>(
-    request: &HTTP::Request<'_, T>,
+    request: &http::Request<'_, T>,
 ) -> Result<TlsStream<TcpStream>, Box<dyn std::error::Error>> {
     let url = format!("{root}:443", root = request.root);
     let tcp_stream = TcpStream::connect(url)?;
@@ -48,7 +47,7 @@ fn initiate_stream_tls<T: std::io::Read>(
 }
 
 // fn initiate_stream_non_tls<T: std::io::Read>(
-//     request: &HTTP::Request<'_, T>,
+//     request: &http::Request<'_, T>,
 // ) -> Result<TcpStream, Box<dyn std::error::Error>> {
 //     let url = format!("{root}:443", root = request.root);
 //     let tcp_stream = TcpStream::connect(url)?;
@@ -57,14 +56,14 @@ fn initiate_stream_tls<T: std::io::Read>(
 
 /// # Errors
 ///
-/// returns an error if the returned HTTP response is invalid
+/// returns an error if the returned http response is invalid
 pub fn make_get_request(
     root: &str,
     path: &str,
-    headers: &HTTP::Headers<'_>,
-) -> Result<HTTP::Response<'static>, Box<dyn std::error::Error>> {
-    let request = HTTP::Request {
-        method: HTTP::Method::GET,
+    headers: &http::Headers<'_>,
+) -> Result<http::Response<'static>, Box<dyn std::error::Error>> {
+    let request = http::Request {
+        method: http::Method::GET,
         root,
         path,
         headers,
@@ -75,16 +74,16 @@ pub fn make_get_request(
 
 /// # Errors
 ///
-/// returns an error if the returned HTTP response is invalid
+/// returns an error if the returned http response is invalid
 pub fn make_request<T: std::io::Read + Send>(
-    mut request: HTTP::Request<'_, T>,
-) -> Result<HTTP::Response<'static>, Box<dyn std::error::Error>> {
+    mut request: http::Request<'_, T>,
+) -> Result<http::Response<'static>, Box<dyn std::error::Error>> {
     let mut stream = initiate_stream_tls(&request)?;
     let _out = std::io::copy(&mut request.content, &mut stream)?;
     parse_http_response(stream)
 
     // TODO monomorphism will 2x this...
-    // if let Ok(ref response) = response && response.code == HTTP::ResponseCode::UPGRADE_REQUIRED {
+    // if let Ok(ref response) = response && response.code == http::ResponseCode::UPGRADE_REQUIRED {
     //     // try non TLS
     //     let mut stream = initiate_stream(&request)?;
     //     let _out = std::io::copy(&mut request.content, &mut stream)?;
@@ -96,12 +95,12 @@ pub fn make_request<T: std::io::Read + Send>(
 
 pub(crate) fn parse_http_response<S: std::io::Read + Send + 'static>(
     stream: S,
-) -> Result<HTTP::Response<'static>, Box<dyn std::error::Error + 'static>> {
+) -> Result<http::Response<'static>, Box<dyn std::error::Error + 'static>> {
     use std::io::{BufRead, BufReader};
 
     let mut reader = BufReader::new(stream);
 
-    let code: HTTP::ResponseCode = {
+    let code: http::ResponseCode = {
         let mut line = String::new();
         let Ok(_bytes_read) = reader.read_line(&mut line) else {
             return Err("no code".into());
@@ -110,7 +109,7 @@ pub(crate) fn parse_http_response<S: std::io::Read + Send + 'static>(
         let code = line
             .trim_end()
             .split_once(' ')
-            .and_then(|(_method, item)| HTTP::ResponseCode::from_line(item).ok());
+            .and_then(|(_method, item)| http::ResponseCode::from_line(item).ok());
 
         let Some(code) = code else {
             return Err(format!("invalid response code: {line:?}").into());
@@ -156,7 +155,7 @@ pub(crate) fn parse_http_response<S: std::io::Read + Send + 'static>(
             match part {
                 "chunked" => {
                     // TODO performance
-                    reader = Box::new(HTTP::ChunkedReader::new(std::io::BufReader::new(reader)));
+                    reader = Box::new(http::ChunkedReader::new(std::io::BufReader::new(reader)));
                 }
                 #[cfg(feature = "decompress")]
                 "gzip" => {
@@ -183,11 +182,11 @@ pub(crate) fn parse_http_response<S: std::io::Read + Send + 'static>(
         }
     }
 
-    let headers = HTTP::Headers::from_string(headers);
+    let headers = http::Headers::from_string(headers);
 
     let body = reader;
 
-    let response = HTTP::Response {
+    let response = http::Response {
         code,
         headers,
         body,
