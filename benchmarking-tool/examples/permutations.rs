b5e54d92d7ec6e62865cd60613191d7896e7a98e
Ben <kaleidawave@gmail.com>
2025-12-20 09:34:09 +0000

    Fix import in example

--- benchmarking-tool/examples/permutations.rs
+++ benchmarking-tool/examples/permutations.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let permutations = Permutations::new(vec![vec![1, 2, 3], vec![10, 20, 30], vec![100, 200]]);
+    let permutations = crate::utilities::Permutations::new(vec![vec![1, 2, 3], vec![10, 20, 30], vec![100, 200]]);
     let mut i = 0;
     for permutation in permutations {
         let prefix = &"     "[..i];
